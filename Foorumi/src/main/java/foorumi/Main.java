a43665877ef6c6ac18ee1552aeb6154dd8bea3a8
Hyytiälä Otto A <hyytiala@da5-cs-b221-08.helsinki.fi>
2016-02-25 10:23:27 +0200

    Lisätty spark toiminnalisuus

--- Foorumi/src/main/java/foorumi/Main.java
+++ Foorumi/src/main/java/foorumi/Main.java
@@ -2,6 +2,9 @@ package foorumi;
 
 import java.sql.*;
 import java.util.*;
+import spark.*;
+import static spark.Spark.*;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
 
@@ -10,15 +13,21 @@ public class Main {
         KeskusteluDao kdao = new KeskusteluDao(db);
         ViestiDao vdao = new ViestiDao(db);
         Keskustelu k = kdao.haeYksi("1", vdao);
-        
+
+        get("/viesti", (req, res) -> {
+            String a = k.getNimi() + "<br/>" + "<br/>";
+            for (Viesti v : k.getViestit()) {
+                a += v.getViesti() + ":" + v.getAikaleima() + ":" + v.getNimimerkki() + "<br/>";
+            }
+            return a;
+        });
+
         System.out.println("*****************");
         System.out.println(k.getNimi() + ":");
-        
+
         for (Viesti v : k.getViestit()) {
             System.out.println("\t" + v.getViesti());
         }
-        
-        
-        
+
     }
 }
