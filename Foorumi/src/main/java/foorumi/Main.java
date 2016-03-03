e852106f44164c9123e3d7e57e06da3c8e1d239c
pullari <samuli.rouvinen@gmail.com>
2016-03-03 15:44:10 +0200

    suhteellisen toimiva versio

--- Foorumi/src/main/java/foorumi/Main.java
+++ Foorumi/src/main/java/foorumi/Main.java
@@ -1,14 +1,7 @@
 package foorumi;
 
-<<<<<<< HEAD
 import java.util.*;
 import spark.*;
-=======
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import spark.ModelAndView;
->>>>>>> 39f15b7b27d14b9ae411278c888b59f94012dc1a
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
@@ -16,7 +9,6 @@ public class Main {
     
     public static void main(String[] args) throws Exception {
         Database db = new Database("jdbc:sqlite:foorumi.db");
-<<<<<<< HEAD
         DAO dao = new DAO(db);
 
         get("/", (req, res) -> {
@@ -74,50 +66,6 @@ public class Main {
                 map.put("viestit", dao.haeKeskustelunViestit(konvo.getId()));
                 return new ModelAndView(map, "viestit");
             }, new ThymeleafTemplateEngine());
-=======
-        KeskusteluDao kdao = new KeskusteluDao(db);
-        ViestiDao vdao = new ViestiDao(db);
-        AlueDao adao = new AlueDao(db);
-        Keskustelu k = kdao.haeYksi("1", vdao);
-        List<Viesti> viestit = k.getViestit();
-        List<Alue> alueet = adao.haeKaikki();
-        String t = "sahko";
-        
-
-        get("/", (req, res) -> {
-            HashMap map = new HashMap<>();
-            map.put("alueet", alueet);
-            return new ModelAndView(map, "index");
-        }, new ThymeleafTemplateEngine());
-        
-        get("/keskustelu", (req, res) -> {
-            HashMap map = new HashMap<>();
-            map.put("viestit", viestit);
-            return new ModelAndView(map, "keskustelu");
-        }, new ThymeleafTemplateEngine());
-        
-        get("/alue/" + t, (req, res) -> {
-            HashMap map = new HashMap<>();
-            map.put("viestit", viestit);
-            return new ModelAndView(map, "keskustelu");
-        }, new ThymeleafTemplateEngine());
-        
-        post("/", (req, res) -> {
-            String nimi = req.queryParams("linkki");
-            return "Kerrotaan siitä tiedon lähettäjälle: " + nimi;
-        });
-
-
-        System.out.println("*****************");
-        System.out.println(k.getNimi() + ":");
-
-        for (Viesti v : k.getViestit()) {
-            System.out.println("\t" + v.getViesti() + ":" + v.getNimimerkki() + ":" + v.getAikaleima());
-        }
-
-        for (Alue a : alueet) {
-            System.out.println(a.getNimi() + ":" + a.getViesteja());
->>>>>>> 39f15b7b27d14b9ae411278c888b59f94012dc1a
         }
     }
 }
